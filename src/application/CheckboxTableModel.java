1baeb205cc9c738002f7cc3b71dd842d69251264
Andrew Kowalczyk <kowandrew@gmail.com>
2018-05-10 01:21:31 +1000

    init comit

---
+++ src/application/CheckboxTableModel.java
@@ -0,0 +1,13 @@
+package application;
+
+public interface CheckboxTableModel {
+
+	public String getName();
+	
+	public void setName(String name);
+	
+	public boolean isChecked();
+	
+	public void setChecked(boolean b);
+	
+}
